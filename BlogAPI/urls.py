e0753712c0894413e7dd8d456cfe4ca9135783c9
Olusegun .O. ADELEKE <oadeleke123@gmail.com>
2024-01-25 19:40:56 +0100

    add: url paths for mixin based views

--- BlogAPI/urls.py
+++ BlogAPI/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from . import views
-from .views import Listblog
+from .views import ListBlog, DetailedBlog, ListBlogsMixin, DetailedBlogMixin
 
 urlpatterns = [
     path('bloglist', views.bloglist, name='bloglist'),
-    path('allbloglist', Listblog.as_view(), name='allbloglist')
+    path('allbloglist', ListBlog.as_view(), name='allbloglist', ),
+    path('blog/<int:id>', DetailedBlog.as_view(), name='detailedblog'),
+    # mixinx views
+    path('mixinallblogs', ListBlogsMixin.as_view(), name='mbv'),
+    path('mixinblog/<int:pk>', DetailedBlogMixin.as_view(), name='dbv'),
 ]
--- BlogAPI/views.py
+++ BlogAPI/views.py
@@ -4,8 +4,9 @@ from .serializers import BlogSerializer, CommentSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status, mixins, generics
 
+# function based views
 # get all blogs
 @api_view(['GET', 'POST'])
 def bloglist(request):
@@ -13,8 +14,8 @@ def bloglist(request):
     serializer_blog = BlogSerializer(all_blogs, many=True)
     return Response(serializer_blog.data)
 
-# class view for list blog
-class Listblog(APIView):
+# class based view for list blog
+class ListBlog(APIView):
     # get request to retrieve all blogs
     def get(self, request):
         all_blogs = Blog.objects.all()
@@ -23,13 +24,74 @@ class Listblog(APIView):
     
     # post request to add a blog to database
     def post(self, request):
+        # use blog serializer to object
         serializer_blog = BlogSerializer(data=request.data)
 
+        # check if object is valid then save object.
         if serializer_blog.is_valid(raise_exception=True):
             blog_saved = serializer_blog.save()
             return Response(f"Success: {blog_saved.title} created successfully...")
+        
+        # return if object serialized is not valid
         return Response(serializer_blog.errors, status=status.HTTP_400_BAD_REQUEST)
     
-# class Detailedblog(APIView):
-#     def get(self, response):
+class DetailedBlog(APIView):
+    # get blog by id
+    def get(self, request, id):
+        blog = Blog.objects.filter(id=id)
+        serializer_blog = BlogSerializer(blog, many=True)
+        return Response(serializer_blog.data)
 
+    # update a blog by id.
+    def put(self, request, id):
+        blog = Blog.objects.get(id=id)
+        serializer_blog = BlogSerializer(blog, request.data)
+        
+        if serializer_blog.is_valid(raise_exception=True):
+            blog_save = serializer_blog.save()
+            return Response(f"Success: {blog_save.title} updated successfully...")
+        
+        return Response(serializer_blog.error, status=status.HTTP_400_BAD_REQUEST)
+    
+    # delete a blog by id
+    def delete(self, request, id):
+        blog = Blog.objects.get(id=id)
+        title = blog.title
+        blog.delete()
+        return Response(f"Success: {title} deleted sucessfully...")
+
+
+# Mixins based views
+# get list of blogs
+class ListBlogsMixin(mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+    # get works with self.list
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+# handle crud operation for individual blog
+class DetailedBlogMixin(mixins.CreateModelMixin,
+                        mixins.RetrieveModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin,
+                        generics.GenericAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+    # get here works with self.retrieve
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    # post here works with self.create
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    #put here works with self.update
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    # delete here works with self.destroy
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
