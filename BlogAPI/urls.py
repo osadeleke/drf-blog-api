515539b7b62a8fd1e26bb3392740b44fd1dc01cf
Olusegun .O. ADELEKE <oadeleke123@gmail.com>
2024-01-25 18:29:40 +0100

    add: using class view to create get and post blogapi request on all blogs

--- BlogAPI/urls.py
+++ BlogAPI/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from .views import Listblog
 
 urlpatterns = [
     path('bloglist', views.bloglist, name='bloglist'),
+    path('allbloglist', Listblog.as_view(), name='allbloglist')
 ]
--- BlogAPI/views.py
+++ BlogAPI/views.py
@@ -3,6 +3,8 @@ from .models import Blog, Comment
 from .serializers import BlogSerializer, CommentSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import status
 
 # get all blogs
 @api_view(['GET', 'POST'])
@@ -10,3 +12,24 @@ def bloglist(request):
     all_blogs = Blog.objects.all()
     serializer_blog = BlogSerializer(all_blogs, many=True)
     return Response(serializer_blog.data)
+
+# class view for list blog
+class Listblog(APIView):
+    # get request to retrieve all blogs
+    def get(self, request):
+        all_blogs = Blog.objects.all()
+        serializer_blog = BlogSerializer(all_blogs, many=True)
+        return Response(serializer_blog.data)
+    
+    # post request to add a blog to database
+    def post(self, request):
+        serializer_blog = BlogSerializer(data=request.data)
+
+        if serializer_blog.is_valid(raise_exception=True):
+            blog_saved = serializer_blog.save()
+            return Response(f"Success: {blog_saved.title} created successfully...")
+        return Response(serializer_blog.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# class Detailedblog(APIView):
+#     def get(self, response):
+
